efeb0a455d1bf82537eff3dd445fabdafd3cce5c
unknown <sanya@example.com>
2024-12-15 22:17:14 +0700

    add_3

--- src/financial_transaction.py
+++ src/financial_transaction.py
@@ -1,33 +1,34 @@
 import csv
-import pandas as pd
 from typing import Any
+
+import pandas as pd
 
 
 def transactions_csv_file(filename: str) -> Any:
-    ''' Функция, считывающая финансовые операции из CSV-файлов '''
+    """Функция, считывающая финансовые операции из CSV-файлов"""
     transactions = []
     try:
-        with open(f'{filename}', encoding="utf-8") as file_csv:
-            reader = csv.DictReader(file_csv, delimiter=';')
+        with open(f"{filename}", encoding="utf-8") as file_csv:
+            reader = csv.DictReader(file_csv, delimiter=";")
             for row in reader:
-                transactions.append(row['description'])
+                transactions.append(row["description"])
             return transactions
     except Exception as error:
-        return f'Ошибка: {error}'
-    return ''
+        return f"Ошибка: {error}"
+    return ""
 
 
-print(transactions_csv_file('..\\files\\transactions.csv'))
+print(transactions_csv_file("..\\files\\transactions.csv"))
 
 
 def transactions_xlsx_file(filename: str) -> Any:
-    ''' Функция, считывающая финансовые операции из XLSX-файлов '''
+    """Функция, считывающая финансовые операции из XLSX-файлов"""
     try:
-        excel_file = pd.read_excel(f'{filename}')
+        excel_file = pd.read_excel(f"{filename}")
         return excel_file
     except Exception as error:
-        return f'Ошибка: {error}'
-    return ''
+        return f"Ошибка: {error}"
+    return ""
 
 
-print(transactions_xlsx_file('..\\files\\transactions_excel.xlsx'))
+print(transactions_xlsx_file("..\\files\\transactions_excel.xlsx"))
