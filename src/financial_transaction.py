8d9536f305eb023b636b133af20298b29d0def29
unknown <sanya@example.com>
2024-12-16 20:12:34 +0700

    add_4

--- src/financial_transaction.py
+++ src/financial_transaction.py
@@ -6,13 +6,13 @@ import pandas as pd
 
 def transactions_csv_file(filename: str) -> Any:
     """Функция, считывающая финансовые операции из CSV-файлов"""
-    transactions = []
+    transactions_csv = []
     try:
         with open(f"{filename}", encoding="utf-8") as file_csv:
             reader = csv.DictReader(file_csv, delimiter=";")
             for row in reader:
-                transactions.append(row["description"])
-            return transactions
+                transactions_csv.append(row)
+            return transactions_csv
     except Exception as error:
         return f"Ошибка: {error}"
     return ""
@@ -25,7 +25,8 @@ def transactions_xlsx_file(filename: str) -> Any:
     """Функция, считывающая финансовые операции из XLSX-файлов"""
     try:
         excel_file = pd.read_excel(f"{filename}")
-        return excel_file
+        transactions_xlsx = excel_file.to_dict(orient="records")
+        return transactions_xlsx
     except Exception as error:
         return f"Ошибка: {error}"
     return ""
